af3358c29f7819b57eff41861f9e2751a30bf7e5
Cecilia Salema <ceciliasiman15@gmail.com>
2023-03-29 13:56:40 -0300

    replace greek letter names with actual letters

--- src/components/dataObjects.js
+++ src/components/dataObjects.js
@@ -1,7 +1,7 @@
 export default function data(number) {
   const star1 = {
     estado: "Pará",
-    estrela: "Spica (alfa de Virgem)",
+    estrela: "Spica (α de Virgem)",
     constelacao:
       "https://www.solarsystemquick.com/universe/virgo-constellation-new2.jpg",
     curiosidade:
@@ -11,7 +11,7 @@ export default function data(number) {
   };
   const star2 = {
     estado: "Amazonas",
-    estrela: "Procyon (alfa do Cão Menor)",
+    estrela: "Procyon (α do Cão Menor)",
     constelacao:
       "https://3.bp.blogspot.com/-fKXQIef_R7U/WL4eb86uu_I/AAAAAAAAAeE/6ty5SOq5aMsGUEtKp57mqGjF-LZLtOwfQCLcB/s400/canis-minor-art.jpg",
     curiosidade:
@@ -19,7 +19,7 @@ export default function data(number) {
   };
   const star3 = {
     estado: "Mato Grosso do Sul",
-    estrela: "Alphard (alfa da Hidra Fêmea)",
+    estrela: "Alphard (α da Hidra Fêmea)",
     constelacao:
       "https://4.bp.blogspot.com/-xTcslAsohOo/XwEmqgUGzxI/AAAAAAAAHLk/KUCKUVxWn8URW__NS5EDrkFT-NYmOOHOgCK4BGAYYCw/s400/Figura03.png",
     curiosidade:
@@ -27,15 +27,15 @@ export default function data(number) {
   };
   const star4 = {
     estado: "Acre",
-    estrela: "Gama da Hidra Fêmea",
+    estrela: "γ da Hidra Fêmea",
     constelacao:
       "https://4.bp.blogspot.com/-xTcslAsohOo/XwEmqgUGzxI/AAAAAAAAHLk/KUCKUVxWn8URW__NS5EDrkFT-NYmOOHOgCK4BGAYYCw/s400/Figura03.png",
     curiosidade:
-      "Apesar de Hidra Fêmea ser uma constelação muito grande (diferente de Hidra Macho, que é muito pequena), apenas a alfa de Hidra é muito brilhante. As demais não recebem muita atenção e não têm curiosidades :(",
+      "Apesar de Hidra Fêmea ser uma constelação muito grande (diferente de Hidra Macho, que é muito pequena), apenas a α de Hidra é muito brilhante. As demais não recebem muita atenção e não têm curiosidades :(",
   };
   const star5 = {
     estado: "Mato Grosso",
-    estrela: "Sirius (alfa do Cão Maior)",
+    estrela: "Sirius (α do Cão Maior)",
     constelacao:
       "http://chc.org.br/wp-content/uploads/2012/01/Olhe-para-o-c%C3%A9u-e-conhe%C3%A7a-constela%C3%A7%C3%B5es-repletas-de-curiosidades.jpg",
     curiosidade:
@@ -43,14 +43,14 @@ export default function data(number) {
   };
   const star6 = {
     estado: "Amapá",
-    estrela: "Mirzam (Beta do Cão Maior)",
+    estrela: "Mirzam (β do Cão Maior)",
     constelacao:
       "https://media.istockphoto.com/id/1085238832/pt/vetorial/canis-major-constellation-vector-illustration-with-the-names-of-basic-stars.jpg?s=612x612&w=0&k=20&c=fU2iSQF_6rTJCDKi8HAtoDnGHVPp7vn4bZVS10GSiZ0=",
     curiosidade: "O nome vem do árabe e significa 'arauto'.",
   };
   const star7 = {
     estado: "Rondônia",
-    estrela: "Muliphen (gama do Cão Maior)",
+    estrela: "Muliphen (γ do Cão Maior)",
     constelacao:
       "https://media.istockphoto.com/id/1085238832/pt/vetorial/canis-major-constellation-vector-illustration-with-the-names-of-basic-stars.jpg?s=612x612&w=0&k=20&c=fU2iSQF_6rTJCDKi8HAtoDnGHVPp7vn4bZVS10GSiZ0=",
     curiosidade:
@@ -58,7 +58,7 @@ export default function data(number) {
   };
   const star8 = {
     estado: "Roraima",
-    estrela: "Wezen (delta do Cão Maior)",
+    estrela: "Wezen (δ do Cão Maior)",
     constelacao:
       "https://media.istockphoto.com/id/1085238832/pt/vetorial/canis-major-constellation-vector-illustration-with-the-names-of-basic-stars.jpg?s=612x612&w=0&k=20&c=fU2iSQF_6rTJCDKi8HAtoDnGHVPp7vn4bZVS10GSiZ0=",
     curiosidade:
@@ -66,7 +66,7 @@ export default function data(number) {
   };
   const star9 = {
     estado: "Tocantins",
-    estrela: "Adhara (epsilon do Cão Maior)",
+    estrela: "Adhara (ε do Cão Maior)",
     constelacao:
       "https://media.istockphoto.com/id/1085238832/pt/vetorial/canis-major-constellation-vector-illustration-with-the-names-of-basic-stars.jpg?s=612x612&w=0&k=20&c=fU2iSQF_6rTJCDKi8HAtoDnGHVPp7vn4bZVS10GSiZ0=",
     curiosidade:
@@ -78,11 +78,11 @@ export default function data(number) {
     constelacao:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/Carina_constellation_map.svg/240px-Carina_constellation_map.svg.png",
     curiosidade:
-      "É a segunda estrela do céu noturno (a primeira está na bandeira também). A estrela é a alfa da constelação Carina (ou quilha), que faz parte da agora obsoleta, gigantesca constelação de Argo Navis, que representava o navio utilizado por Jasão e os Argonautas. À estrela mais brilhante da constelação foi dado o nome do piloto do navio da lenda grega — Canopus foi o piloto do navio de Menelau em sua expedição para reaver Helena de Troia depois dela ter sido levada por Páris. A estrela é a única representante da constelação na bandeira.",
+      "É a segunda estrela do céu noturno (a primeira está na bandeira também). A estrela é a α da constelação Carina (ou quilha), que faz parte da agora obsoleta, gigantesca constelação de Argo Navis, que representava o navio utilizado por Jasão e os Argonautas. À estrela mais brilhante da constelação foi dado o nome do piloto do navio da lenda grega — Canopus foi o piloto do navio de Menelau em sua expedição para reaver Helena de Troia depois dela ter sido levada por Páris. A estrela é a única representante da constelação na bandeira.",
   };
   const star11 = {
     estado: "Bahia",
-    estrela: "Gama do Cruzeiro do Sul",
+    estrela: "γ do Cruzeiro do Sul",
     constelacao:
       "https://star-name-registry.com/modules/starconstpg/views/img/new/crux-map-m.jpg",
     curiosidade:
@@ -90,7 +90,7 @@ export default function data(number) {
   };
   const star12 = {
     estado: "Minas Gerais",
-    estrela: "Pálida (delta do Cruzeiro do Sul)",
+    estrela: "Pálida (δ do Cruzeiro do Sul)",
     constelacao:
       "https://star-name-registry.com/modules/starconstpg/views/img/new/crux-map-m.jpg",
     curiosidade:
@@ -98,7 +98,7 @@ export default function data(number) {
   };
   const star13 = {
     estado: "Espírito Santo",
-    estrela: "Intrometida (epsilon do Cruzeiro do Sul)",
+    estrela: "Intrometida (ε do Cruzeiro do Sul)",
     constelacao:
       "https://star-name-registry.com/modules/starconstpg/views/img/new/crux-map-m.jpg",
     curiosidade:
@@ -110,11 +110,11 @@ export default function data(number) {
     constelacao:
       "https://star-name-registry.com/modules/starconstpg/views/img/new/crux-map-m.jpg",
     curiosidade:
-      "Chamada de Acrux ou Estrela de Magalhães (em homenagem ao navegante português Fernão de Magalhães), a alfa do Cruzeiro do Sul é a 13ª estrela mais brilhante do céu noturno. Acrux é um sistema estelar múltiplo composto por pelo menos três estrelas.",
+      "Chamada de Acrux ou Estrela de Magalhães (em homenagem ao navegante português Fernão de Magalhães), a α do Cruzeiro do Sul é a 13ª estrela mais brilhante do céu noturno. Acrux é um sistema estelar múltiplo composto por pelo menos três estrelas.",
   };
   const star15 = {
     estado: "Rio de Janeiro",
-    estrela: "Mimosa (beta do Cruzeiro do Sul)",
+    estrela: "Mimosa (β do Cruzeiro do Sul)",
     constelacao:
       "https://star-name-registry.com/modules/starconstpg/views/img/new/crux-map-m.jpg",
     curiosidade:
@@ -124,22 +124,22 @@ export default function data(number) {
   };
   const star16 = {
     estado: "Piauí",
-    estrela: "Antares (alfa de Escorpião)",
+    estrela: "Antares (α de Escorpião)",
     constelacao:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Scorpius_constellation_map.svg/800px-Scorpius_constellation_map.svg.png",
     curiosidade:
-      "Seu nome significa 'anti-Ares'. Ares é o nome grego do deus romano Marte. Como a alfa de escorpião é avermelhada e brilhante, bem como o planeta Marte, os corpos celestes rivalizam.",
+      "Seu nome significa 'anti-Ares'. Ares é o nome grego do deus romano Marte. Como a α de escorpião é avermelhada e brilhante, bem como o planeta Marte, os corpos celestes rivalizam.",
   };
   const star17 = {
     estado: "Maranhão",
-    estrela: "Acrab (beta de Escorpião)",
+    estrela: "Acrab (β de Escorpião)",
     constelacao:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Scorpius_constellation_map.svg/800px-Scorpius_constellation_map.svg.png",
     curiosidade: "Também é conhecida como Graffias, 'a pinça' em grego.",
   };
   const star18 = {
     estado: "Ceará",
-    estrela: "Larawag (epsilon de Escorpião)",
+    estrela: "Larawag (ε de Escorpião)",
     constelacao:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Scorpius_constellation_map.svg/800px-Scorpius_constellation_map.svg.png",
     curiosidade:
@@ -147,7 +147,7 @@ export default function data(number) {
   };
   const star19 = {
     estado: "Rio Grande do Norte",
-    estrela: "Shaula (lambda de Escorpião)",
+    estrela: "Shaula (λ de Escorpião)",
     constelacao:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Scorpius_constellation_map.svg/800px-Scorpius_constellation_map.svg.png",
     curiosidade:
@@ -155,21 +155,21 @@ export default function data(number) {
   };
   const star20 = {
     estado: "Paraíba",
-    estrela: "Capa de Escorpião",
+    estrela: "k de Escorpião",
     constelacao:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Scorpius_constellation_map.svg/800px-Scorpius_constellation_map.svg.png",
     curiosidade: "",
   };
   const star21 = {
     estado: "Pernambuco",
-    estrela: "Mu de Escorpião",
+    estrela: "μ de Escorpião",
     constelacao:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Scorpius_constellation_map.svg/800px-Scorpius_constellation_map.svg.png",
     curiosidade: "O nome da estrela é Denebakrab.",
   };
   const star22 = {
     estado: "Alagoas",
-    estrela: "Sargas (theta de Escorpião)",
+    estrela: "Sargas (θ de Escorpião)",
     constelacao:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Scorpius_constellation_map.svg/800px-Scorpius_constellation_map.svg.png",
     curiosidade:
@@ -177,14 +177,14 @@ export default function data(number) {
   };
   const star23 = {
     estado: "Sergipe",
-    estrela: "Iota de Escorpião",
+    estrela: "ι de Escorpião",
     constelacao:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Scorpius_constellation_map.svg/800px-Scorpius_constellation_map.svg.png",
     curiosidade: "",
   };
   const star24 = {
     estado: "Santa Catarina",
-    estrela: "Beta do Triângulo Austral",
+    estrela: "β do Triângulo Austral",
     constelacao:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Triangulum_Australe_constellation_map.svg/240px-Triangulum_Australe_constellation_map.svg.png",
     curiosidade:
@@ -192,7 +192,7 @@ export default function data(number) {
   };
   const star25 = {
     estado: "Rio Grande do Sul",
-    estrela: "Alfa do Triângulo Austral",
+    estrela: "α do Triângulo Austral",
     constelacao:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Triangulum_Australe_constellation_map.svg/240px-Triangulum_Australe_constellation_map.svg.png",
     curiosidade:
@@ -200,7 +200,7 @@ export default function data(number) {
   };
   const star26 = {
     estado: "Paraná",
-    estrela: "Gama do Triângulo Austral",
+    estrela: "γ do Triângulo Austral",
     constelacao:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Triangulum_Australe_constellation_map.svg/240px-Triangulum_Australe_constellation_map.svg.png",
     curiosidade: "É conhecida por alguns como Gatria"
@@ -211,7 +211,7 @@ export default function data(number) {
     constelacao:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Octans_constellation_PP3_map_PL.svg/240px-Octans_constellation_PP3_map_PL.svg.png",
     curiosidade:
-      "É a estrela sigma da constelação de Oitante, e a única estrela dessa constelação na bandeira. Também chamada de Polaris Australis ou Estrela Polar do Sul, em virtude de ser a mais próxima do pólo celeste sul. Simboliza que, da mesma forma que todas as estrelas do hemisfério celeste sul giram em torno do eixo próximo a Sigma Octantis, os Estados do Brasil estão em volta do Distrito Federal.",
+      "É a estrela sigma σ da constelação de Oitante, e a única estrela dessa constelação na bandeira. Também chamada de Polaris Australis ou Estrela Polar do Sul, em virtude de ser a mais próxima do pólo celeste sul. Simboliza que, da mesma forma que todas as estrelas do hemisfério celeste sul giram em torno do eixo próximo a Sigma Octantis, os Estados do Brasil estão em volta do Distrito Federal.",
   };
   const reference = {
     1: star1,
